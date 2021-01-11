381bf58d1653414bec2ae1c44628400af17fbdaa
YourSandwich <norbert23pl@gmail.com>
2021-01-11 19:33:23 +0100

    d

--- preinstall.sh
+++ preinstall.sh
@@ -84,6 +84,7 @@ echo "-- Grub Installation  --"
 echo "--------------------------------------"
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "--------------------------------------"
 echo "--          Network Setup           --"
