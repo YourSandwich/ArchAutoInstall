62eea378e9bc855a2da8c2f69e6c34438e40e635
YourSandwich <norbert23pl@gmail.com>
2021-01-11 22:11:45 +0100

    typo fix

--- preinstall.sh
+++ preinstall.sh
@@ -87,7 +87,7 @@ ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
 hwclock --systohc
 #sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
 #sed -i "s/#de_AT.UTF-8/de_AT.UTF-8/g" /etc/locale.gen
-locale-gen
+#locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=de-latin1-nodeadkeys" >> /etc/vconsole.conf
 #echo "ReRe" >> /etc/hostname
